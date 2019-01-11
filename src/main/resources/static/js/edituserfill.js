d8d7b1e0b52720aed1463e1a3f836a4f227a92d0
Mikolla <miqolay@gmail.com>
2019-01-11 19:21:59 +0500

    ajax edit user save

--- src/main/resources/static/js/edituserfill.js
+++ src/main/resources/static/js/edituserfill.js
@@ -23,7 +23,7 @@ $(document).on("click", '#edButton', function() {
     console.log('password = ' + password100);
     console.log('role = ' + role100);
     var modalBody = $('<div id="modalContent"></div>');
-    var modalForm = $('<form id="formoid" role="form" name="modalForm" action="/rest/" method="PUT" ></form>');
+    var modalForm = $('<form id="formoid" role="form" name="modalForm" action="/test" method="POST" ></form>');
 
 
     var idForm = $('<div class="form-group"></div>');
@@ -31,17 +31,17 @@ $(document).on("click", '#edButton', function() {
     idForm.append('<label for="id" style="margin-top: 5%">id</label>');
     idForm.append('<input class="form-control"  disabled="disabled"  name="id" id="id" value="'+id100+'" />');
 
-    idForm.append('<input class="form-control"  type="hidden"  name="id" id="id" value="'+id100+'" />');
+    idForm.append('<input class="form-control"  type="hidden"  name="id" id="id" value="'+id100+'" ref="id"/>');
 
 
     idForm.append('<label for="'+columnHeadings[1]+'" style="margin-top: 5%">login</label>');
-    idForm.append('<input class="form-control" name="login" id="login" value="'+login100+'" />');
+    idForm.append('<input class="form-control" name="login" id="login" value="'+login100+'" ref="login"/>');
 
     idForm.append('<label for="'+columnHeadings[2]+'" style="margin-top: 5%">name</label>');
-    idForm.append('<input class="form-control" name="name" id="name" value="'+name100+'" />');
+    idForm.append('<input class="form-control" name="name" id="name" value="'+name100+'" ref="name" />');
 
     idForm.append('<label for="'+columnHeadings[3]+'" style="margin-top: 5%">password</label>');
-    idForm.append('<input class="form-control" name="password" id="password" value="'+password100+'" />');
+    idForm.append('<input class="form-control" name="password" id="password" value="'+password100+'" ref="password" />');
 
     var sel = $('<select class="form-control" name="role" id="role"></select>');
     if (role100 == 'Admin') {
@@ -74,34 +74,54 @@ $(document).on("click", '.btn-primary', function() {
     var fd = new FormData(myform);
 
     console.log('formdata - ' + fd);
+    console.log('id - ' + fd.get('login'));
+
+    var queryString = $('#formoid').serialize();
+    console.log(queryString);
+
+
+    var formEl = document.forms.formoid;
+    var formData = new FormData(formEl);
+    var id2 = formData.get('id');
+    var name2 = formData.get('name');
+    var login2 = formData.get('login');
+    var password2 = formData.get('password');
+    var role2 = formData.get('role');
+    console.log('id = ' + id2);
+    console.log('name = ' + name2);
+    console.log('login = ' + login2);
+    console.log('password = ' + password2);
+    console.log('role = ' + role2);
+    var rolid;
+    if (role2 == 'User' ) { rolid = 2;}
+    else if (role2 == 'Admin' ) {rolid = 1;}
+
+ // var jsondata = '{"id":55,"name":"13115","login":"13115","password":"13115","roles":[{"id":2,"roleName":"User"}]}';
+  var jsondata = '{"id":'+ id2 +',"name":"'+ name2 +'","login":"'+ login2 +'","password":"'+ password2 +'","roles":[{"id":' + rolid + ',"roleName":"'+ role2 +'"}]}';
+  console.log(jsondata);
+
 
     $.ajax({
-        url: "/rest",
-        data: fd,
-        cache: false,
-        processData: false,
-        contentType: false,
-        type: 'PUT',
-        success: function (dataofconfirm) {
-           console.log('data saved')
-        }
+        type: 'PUT', // Use POST with X-HTTP-Method-Override or a straight PUT if appropriate.
+        dataType: "json", // Set datatype - affects Accept header
+        contentType: "application/json; charset=utf-8",
+        url: "http://localhost:8080/rest", // A valid URL
+        data: jsondata, // Some data e.g. Valid JSON as a string
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+                                        }
+
+    }).then(function(data) {
+
+        $('form[name="modalForm"]').submit();
     });
 
 
-
-    /*
-    $.ajax({
-    type: 'POST', // Use POST with X-HTTP-Method-Override or a straight PUT if appropriate.
-    dataType: 'json', // Set datatype - affects Accept header
-    url: "http://example.com/people/1", // A valid URL
-    headers: {"X-HTTP-Method-Override": "PUT"}, // X-HTTP-Method-Override set to PUT.
-    data: '{"name": "Dave"}' // Some data e.g. Valid JSON as a string
-});
-     */
 
 });
 
 $('.modal-footer .btn-primary').click(function() {
     console.log('save button clicked')
-    $('form[name="modalForm"]').submit();
+   // $('form[name="modalForm"]').submit();
 });
